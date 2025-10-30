b50adaaa7ad08cd8846173b7716006933e8352e2
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-10-30 15:12:02 +0530

    day-30

---
+++ daily-classes/day-30/script.js
@@ -0,0 +1,14 @@
+// let obj = {};
+// console.log(obj?.name?.first);
+
+// console.log(a);
+// var a = 12;
+// only happen with variable var as var variables are initialized as undefined.
+// but let and const variable are hoisted but not initialized - they are placed in Temporal Dead Zone (TDZ).
+
+// console.log(a); // undefined ✅ (var is hoisted & initialized)
+// var a = 10;
+
+// console.log(b); // ❌ ReferenceError (in TDZ)
+// let b = 20;
+
