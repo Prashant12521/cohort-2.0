471584e9a8f043989d43c1c50890fde742795d7f
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-11-02 13:45:21 +0530

    day-33

---
+++ daily-classes/day-33(practice)/script.js
@@ -0,0 +1,50 @@
+//Level 1 - Pure Beginner Practice
+// 1. Print no from 1-10
+// for(let i = 1; i<11; i++){
+//   console.log(i);
+// }
+
+// 2. Print only even numbers form 1 to 20
+// Use a loop and condition to print only even ones.
+// for(let i=1; i<21; i++){
+//   if(i%2 === 0){
+//   console.log(i);
+//   }
+// }
+
+// 3. Print numbers form 10-1
+// for(let i=10; i>0; i--){
+//   console.log(i);
+// }
+
+// 4. Print the word "Yes" 5 times.
+// for(let i=1; i<6; i++){
+//   console.log('Yes');
+// }
+
+// 5. Print whether numbers from 1 to 10 are even or odd
+// for(let i=1; i<11; i++){
+//   i%2===0 ? console.log(`${i} is even`) : console.log(`${i} is odd`);
+// }
+
+// 6. Ask user for a number and say number is positive or negative
+// let number = +prompt('Number?');
+// number>=0 ? console.log(`${number} is +ve`) : console.log(`${number} is -ve`)
+//parseInt('12') -> 12
+//Number('12') -> 12
+
+// 7. Ask user's age and check if eligible to vote; If age >= 18 -> "Eligible", else -> "Not eligible"
+// let age = prompt('number');
+// if(age===null){
+//   console.log('You cancelled it');
+// }else{
+//   if(age.trim() === ""){
+//     console.log('Space');
+//   }
+//   age = Number(age.trim());
+//   if(isNaN(age)){
+//     console.er('Write Number');
+//   } else{
+//     console.log('number h');
+//   }
+// }
