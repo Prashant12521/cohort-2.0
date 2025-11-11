f57e48f8afc0e95d74bcd8173ba02a5dd43c13b7
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-11-11 14:45:41 +0530

    day-38

--- JS notes/script.js
+++ JS notes/script.js
@@ -6,3 +6,12 @@
 //     console.log(`${i} is odd`);
 //   }
 // }
+
+var a = 10;
+let b = 20;
+{
+  var a = 30; // same var, overwrites outer a
+  let b = 40; // block-scoped, new variable
+  console.log(a, b); // 30 40
+}
+console.log(a, b); // 30 20
