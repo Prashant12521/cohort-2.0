534ee3ee6516e49c9149964827161f461ce4f1d2
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-12-11 22:55:47 +0530

    task-21-mac-desktop

---
+++ tasks/task-21/script.js
@@ -0,0 +1,29 @@
+const wallpaper = document.querySelector(".wrapper");
+const rtClick = document.querySelector("#js-rtClick");
+let rtClickDisplay = 1;
+
+wallpaper.addEventListener("click", function (val) {
+  if (rtClickDisplay === 1) {
+    rtClickDisplay++;
+    rtClick.style.display = "block";
+    rtClick.style.top = val.clientY + "px";
+    rtClick.style.left = val.clientX + "px";
+  } else {
+    rtClickDisplay--;
+    rtClick.style.display = "none";
+  }
+});
+
+const close = document.querySelector("#close");
+const browser = document.querySelector("#js-chrome");
+
+close.addEventListener("click", function () {
+  browser.style.display = "none";
+});
+
+const browserDock = document.querySelector('#chrome-dock');
+
+browserDock.addEventListener('click', function(e){
+  e.stopPropagation();
+  browser.style.display = "block";
+})
