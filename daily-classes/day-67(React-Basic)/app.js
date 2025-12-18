efd731ddcfc92617f49c0846cc870429023ef875
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-12-18 15:49:00 +0530

    day-67(Import-Export-of-JS)

---
+++ daily-classes/day-67(React-Basic)/app.js
@@ -0,0 +1,10 @@
+const name = "kira";
+
+export default name;
+
+export const age = 23;
+
+export function user () {
+  console.log('Nick');
+  return 32
+}
---
+++ daily-classes/day-67(React-Basic)/script.js
@@ -0,0 +1,7 @@
+import name from "./app.js";
+import { age } from "./app.js";
+import { user } from "./app.js";
+
+console.log(name);
+console.log(age);
+console.log(user());
