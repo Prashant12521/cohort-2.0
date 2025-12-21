4f81e64db3234d18b84e31dd935683a942bf538f
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-12-21 23:48:52 +0530

    day-70-react-tailwind

---
+++ daily-classes/day-70(react-tailwindcss)/src/components/First.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div className='h-[300px] w-[300px] bg-red-500'></div>
+  )
+}
+
+export default First
---
+++ daily-classes/day-70(react-tailwindcss)/src/components/Third.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Third = () => {
+  return (
+    <div className='h-[100px] w-[100px] bg-orange-400'></div>
+  )
+}
+
+export default Third 
