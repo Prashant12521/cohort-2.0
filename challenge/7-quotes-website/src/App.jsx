adfffd57f025e01e0c4b79aea44eb19b38125e4d
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2025-12-31 01:13:19 +0530

    localStorage-in-quotes-website

--- challenge/7-quotes-website/src/App.jsx
+++ challenge/7-quotes-website/src/App.jsx
@@ -5,7 +5,22 @@ const App = () => {
   const [quote, setQuote] = useState("");
   const [name, setName] = useState("");
 
-  const [allQuotes, setAllQuotes] = useState([]);
+  const userData = JSON.parse(localStorage.getItem('savedData')) || []
+
+  const [allQuotes, setAllQuotes] = useState(userData);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const userData = [...allQuotes, { quote, name }];
+
+    setAllQuotes(userData)
+    
+    localStorage.setItem('savedData', JSON.stringify(userData))
+
+    setName("");
+    setQuote("");
+  };
 
   const deleteHandler = (idx) => {
     const copyAllQuotes = [...allQuotes];
@@ -13,21 +28,16 @@ const App = () => {
     copyAllQuotes.splice(idx, 1);
 
     setAllQuotes(copyAllQuotes);
+
+    localStorage.setItem('savedData', JSON.stringify(copyAllQuotes))
   };
 
   return (
-    <div className="flex h-screen justify-between bg-black p-4">
-      <div className="flex h-full items-end justify-center">
+    <div className="flex min-h-screen justify-end bg-black p-4">
+      <div className="fixed left-2">
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-
-            setAllQuotes([...allQuotes, { quote, name }]);
-
-            setName("");
-            setQuote("");
-          }}
-          className="flex min-w-[250px] flex-col items-center gap-3 rounded-2xl border bg-gray-500 p-5"
+  onSubmit={submitHandler}
+          className="flex min-w-62.5 flex-col items-center gap-3 rounded-2xl border bg-gray-500 p-5"
         >
           <input
             value={name}
