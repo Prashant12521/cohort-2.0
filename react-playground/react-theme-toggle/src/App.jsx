db8eb2901caa4ef6013217ebc84e6fd68dedcc98
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-01-17 15:21:57 +0530

    react-theme-toggle

---
+++ react-playground/react-theme-toggle/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import useLocalStorage from "use-local-storage";
+
+import "./App.css";
+import { Toggle } from "./components/Toggle";
+
+export const App = () => {
+  // const [isDark, setIsDark] = useState(false);
+  
+  // const [isDark, setIsDark] = useLocalStorage("isDark", false);
+
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
+  return (
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+      <h1 className="title">Hello world!</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
+    </div>
+  );
+};
