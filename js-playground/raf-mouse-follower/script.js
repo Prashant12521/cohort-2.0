2269c5d670d901c62df043ecc9a0ad1fbf79fbc0
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-01-30 22:08:41 +0530

    feat: implement smooth mouse follower using requestAnimationFrame

---
+++ js-playground/raf-mouse-follower/script.js
@@ -0,0 +1,23 @@
+const mouseFollower = document.querySelector(".mouse-follower");
+
+let x = 0,
+  y = 0;
+
+addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+
+  x = clientX;
+  y = clientY;
+
+  far();
+});
+
+function far() {
+  mouseFollower.style.transform = `translate(${x}px,${y}px)`;
+
+  // console.log('hello')
+
+  requestAnimationFrame(far)
+}
+
+far()
